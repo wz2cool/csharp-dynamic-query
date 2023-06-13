10ad0eb5579c5b404ab910116ffcd9aa6960bac7
frank <wz2cool@live.cn>
2023-06-13 08:57:14 +0800

    add

---
+++ src/CsharpDynamicQuery/Models/SortDirection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDynamicQuery.Models
+{
+    /// <summary>
+    /// Specifies the direction of a sort operation.
+    /// </summary>
+    public enum SortDirection
+    {
+        /// <summary>
+        /// Sorts in ascending order.
+        /// </summary>
+        Asc,
+        /// <summary>
+        /// Sorts in descending order.
+        /// </summary>
+        Desc,
+    }
+}
